eb51420717aad9af54a4db1ca60baebc99704bb6
Evan Arnanda <evanarnanda@gmail.com>
2019-12-01 20:03:22 +0700

    ADD PUT, PATCH, DELETE on API VIEWS

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from profiles_api import serializers
+
+
+class HelloAPIView(APIView):
+    """Test API view"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format = None):
+        """Return a list of APIView features"""
+        an_apiview = [
+        'Uses HTTP methods as function (get, put, post, patch, delete)',
+        'Tapi Evan Ganteng banget :) '
+        ]
+
+        return Response({'massage' : 'hello', 'an_apiview' : an_apiview})
+
+    def post(self, request):
+        """Create a hello massage with user name"""
+        serializer = self.serializer_class(data = request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            massage = f'Hello {name}'
+            return Response({'massage' : massage})
+        else:
+            return Response(
+                serializer.errors,
+                status = status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk = None):
+        """Handle updating an object"""
+        return Response({'message' : 'PUT'})
+
+    def patch(self, request, pk = None):
+        """Handle partial update of an object"""
+        return Response({'massage' : 'PATCH'})
+
+    def delete(self, request, pk = None):
+        """Delete an object"""
+        return Response({'massage' : 'DELETE'})
